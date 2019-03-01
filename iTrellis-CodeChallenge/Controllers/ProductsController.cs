6f4d5a2e2316520776fe3a1e681c30deeca9e382
Andrew Leonardi <ndrewleonardi2@gmail.com>
2019-03-01 14:13:16 -0800

    Initialized repo

---
+++ iTrellis-CodeChallenge/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iTrellis_CodeChallenge.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iTrellis_CodeChallenge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            Product product1 = new Product(1, "fugiat exercitation adipisicing", 43, false, 13);
+            Product product2 = new Product(2, "mollit cupidatat Lorum", 70, true, 18);
+            Product product3 = new Product(3, "non quis sint", 33, false, 15);
+            Product product4 = new Product(4, "voluptate cupidatat non", 52, true, 18);
+            Product product5 = new Product(5, "anim amet occaecat", 39, true, 19);
+            Product product6 = new Product(6, "cillum deserunt elit", 47, false, 20);
+            Product product7 = new Product(7, "adipiscing reprehenderit et", 71, false, 15);
+            Product product8 = new Product(8, "ex mollit laboris", 80, false, 15);
+            return new Product[] {
+                product1,
+                product2,
+                product3,
+                product4,
+                product5,
+                product6,
+                product7,
+                product8
+            };
+        }
+    }
+}
---
+++ iTrellis-CodeChallenge/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iTrellis_CodeChallenge.Models
+{
+    public class Product
+    {
+        public Product(int productID, String productName, int inventoryQuantity, Boolean shipOnWeekends, int maxBusinessDaysToShip)
+        {
+            this.productID = productID;
+            this.productName = productName;
+            this.inventoryQuantity = inventoryQuantity;
+            this.shipOnWeekends = shipOnWeekends;
+            this.maxBusinessDaysToShip = maxBusinessDaysToShip;
+        }
+        public int productID { get; set; }
+        public String productName { get; set; }
+        public int inventoryQuantity { get; set; }
+        public Boolean shipOnWeekends { get; set; }
+        public int maxBusinessDaysToShip { get; set; }
+    }
+}
